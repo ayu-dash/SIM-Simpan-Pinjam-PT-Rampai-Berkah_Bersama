b14dbd61495e80b974f8c997d1e909b4cae09751
ayudash <adrianyudhaswara@gmail.com>
2025-12-14 18:06:16 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
+$host = 'db.fr-pari1.bengt.wasmernet.com';
 $db   = 'koperasi_rbb'; 
-$user = 'root';              
-$pass = ''; 
+$user = 'e981327a74758000174a6fc8d572';              
+$pass = '0693e981-327b-7062-8000-f447b0597729'; 
+$port = '10272'
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
